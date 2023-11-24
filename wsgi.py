2478d0739ed219cee902e9576ed3d2c32dc75478
shivamPR2022 <99947112+shivamPR2022@users.noreply.github.com>
2023-11-24 14:12:18 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,3 +3,8 @@ from app import app
 
 if __name__ == "__main__":
     app.run()
+
+host = os.environ.get("HOST", "0.0.0.0")
+    port = os.environ.get("PORT", 5000)
+
+    app.run(host=host, port=port)
